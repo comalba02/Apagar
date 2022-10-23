05806e94fc9b2932e96ad318923d7bd31a20e013
Marco Coronado <comalba02@hotmail.com>
2022-10-23 13:28:54 -0500

    se crea el proyecto

---
+++ src/apagar/Apagar.java
@@ -0,0 +1,19 @@
+package apagar;
+
+/**
+ *
+ * @author Marco Coronado
+ */
+public class Apagar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Vista interfaz = new Vista();
+        interfaz.setVisible(true);
+        
+    }
+    
+}
